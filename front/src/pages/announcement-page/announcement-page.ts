54d7b88aac3bf83468b99b2db613af4803d1129e
Yassin Zairi <yassin.rey200@gmail.com>
2025-11-14 15:29:55 +0100

    Page navigation

---
+++ front/src/pages/announcement-page/announcement-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './announcement-page.html',
+  styleUrl: './announcement-page.css',
+})
+export class AnnouncementPage {}
