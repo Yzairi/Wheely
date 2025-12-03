3ab79fb79560444f577e2253d11be4088b016cdb
Antoine Cairey <cairey.antoine@gmail.com>
2025-12-03 16:13:14 +0100

    backend filtre sur car (sur dates recherchées)

--- back/rentals/views.py
+++ back/rentals/views.py
@@ -1,13 +1,17 @@
 from rest_framework import permissions, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Car, Rental
 from .serializers import CarSerializer, RentalSerializer
+from .filters import CarFilter
 
 
 class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CarFilter
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- back/wheely_back/settings.py
+++ back/wheely_back/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 AUTH_USER_MODEL = "accounts.User"
