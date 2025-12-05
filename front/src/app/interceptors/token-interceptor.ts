d06396f5986819499ba1e133252134d6710d634d
Antoine Cairey <cairey.antoine@gmail.com>
2025-12-05 11:37:10 +0100

    recherche de location front + gestion du refresh token

--- front/src/app/interceptors/token-interceptor.ts
+++ front/src/app/interceptors/token-interceptor.ts
@@ -1,15 +1,41 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Auth } from '../services/auth';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('access');
+  const auth = inject(Auth);
+  const access = localStorage.getItem('access');
+  let authReq = req;
 
-  if (!token) return next(req);
+  // Ajout du token si présent
+  if (access) {
+    authReq = req.clone({
+      setHeaders: { Authorization: `Bearer ${access}` },
+    });
+  }
 
-  const authReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      // Si 401 → essayer refresh token
+      if (error.status === 401) {
+        return auth.refresh().pipe(
+          switchMap(() => {
+            const newAccess = localStorage.getItem('access');
+            const retryReq = req.clone({
+              setHeaders: { Authorization: `Bearer ${newAccess}` },
+            });
+            return next(retryReq);
+          }),
+          catchError((err) => {
+            // Si refresh échoue → logout
+            auth.logout();
+            return throwError(() => err);
+          })
+        );
+      }
 
-  return next(authReq);
+      return throwError(() => error);
+    })
+  );
 };
--- front/src/app/pages/home/home.ts
+++ front/src/app/pages/home/home.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrl: './home.css',
 })
 export class Home {
+  private apiUrl = 'http://localhost:8000/api';
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private http = inject(HttpClient);
+  cars = signal<any>([]);
 
+  dateRangeValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const start = control.get('start_date')?.value;
+      const end = control.get('end_date')?.value;
+      if (!start || !end) return null;
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+      return endDate > startDate ? null : { dateRangeInvalid: true };
+    };
+  }
+
+  form = this.fb.group(
+    {
+      city: [''],
+      start_date: ['', Validators.required],
+      end_date: ['', Validators.required],
+    },
+    { validators: this.dateRangeValidator() }
+  );
+
+  searchAvailableCar() {
+    return this.http
+      .get(
+        `${this.apiUrl}/rentals/cars?start_date=${this.form.value.start_date}&end_date=${this.form.value.end_date}&is_available=true`
+      )
+      .subscribe((data) => this.cars.set(data));
+  }
 }
