18cf323c0a6a34a7d67c8b6d4b09eac79fcac862
Yassin Zairi <yassin.rey200@gmail.com>
2026-03-26 01:02:09 +0100

    fix

---
+++ back/rentals/migrations/0008_merge_0007_comment_rating_0007_location.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("rentals", "0007_comment_rating"),
+        ("rentals", "0007_remove_car_lat_remove_car_lon_car_address_and_more"),
+    ]
+
+    operations = []
